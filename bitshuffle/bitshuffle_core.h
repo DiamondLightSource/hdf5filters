a9db3a4e133ee4069491c4a2d2399bc1427b9317
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2018-07-12 16:19:21 +0100

    bitshuffle: fixing build error with re-declaration of standard types
    stdint.h *is* available on our platform but the __STDC_VERSION__ gcc precompile macro does not appear to be present.

--- bitshuffle/bitshuffle_core.h
+++ bitshuffle/bitshuffle_core.h
@@ -28,18 +28,7 @@
 #ifndef BITSHUFFLE_CORE_H
 #define BITSHUFFLE_CORE_H
 
-// We assume GNU g++ defining `__cplusplus` has stdint.h
-#if (defined (__STDC_VERSION__) && __STDC_VERSION__ >= 199900L) || defined(__cplusplus)
 #include <stdint.h>
-#else
-  typedef unsigned char       uint8_t;
-  typedef unsigned short      uint16_t;
-  typedef unsigned int        uint32_t;
-  typedef   signed int        int32_t;
-  typedef unsigned long long  uint64_t;
-  typedef long long           int64_t;
-#endif
-
 #include <stdlib.h>
 
 
